ea042c4859b616cd5cd7ba70cb590810498a4411
Uniovi283728 <113524917+Uniovi283728@users.noreply.github.com>
2022-11-02 14:23:43 +0100

    Ejercicio1

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estándares en la Web";
+asignatura.titulacion = "Grado en Ingeniería Informática del Software";
+asignatura.centro = "Escuela de Ingeniería Informática";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2021-2022";
+asignatura.estudiante = "Eloy Martin Reig";
+asignatura.email = "uo283728@uniovi.es";
